246577b91fd31666204a089da32a515f06c0967d
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-11-26 23:44:59 -0300

    fix: esconder documentação e adicionar vídeo

--- src/pages/Presentation/sections/Information.js
+++ src/pages/Presentation/sections/Information.js
@@ -24,8 +24,7 @@ import MKBox from "components/MKBox";
 import RotatingCard from "examples/Cards/RotatingCard";
 import RotatingCardFront from "examples/Cards/RotatingCard/RotatingCardFront";
 import RotatingCardBack from "examples/Cards/RotatingCard/RotatingCardBack";
-import DefaultInfoCard from "examples/Cards/InfoCards/DefaultInfoCard";
-
+/* import DefaultInfoCard from "examples/Cards/InfoCards/DefaultInfoCard";*/
 // Images
 import bgFront from "assets/images/rotating-card-bg-front.jpeg";
 import bgBack from "assets/images/rotating-card-bg-back.jpeg";
@@ -35,6 +34,25 @@ function Information() {
     <MKBox component="section" py={6} my={6}>
       <Container>
         <Grid container item xs={11} spacing={3} alignItems="center" sx={{ mx: "auto" }}>
+          <Grid
+            item
+            sx={{
+              ml: "auto",
+              padding: 0,
+              borderRadius: "10px",
+              border: "10px solid #000",
+            }}
+          >
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/Yhp7DX8t660?si=WsG_O_LGBsgc98ld"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            />
+          </Grid>
           <Grid item xs={12} lg={4} sx={{ mx: "auto" }}>
             <RotatingCard>
               <RotatingCardFront
@@ -61,7 +79,7 @@ function Information() {
               />
             </RotatingCard>
           </Grid>
-          <Grid item xs={12} lg={7} sx={{ ml: "auto" }}>
+          {/*  <Grid item xs={12} lg={7} sx={{ ml: "auto" }}>
             <Grid container spacing={3}>
               <Grid item xs={12} md={6}>
                 <DefaultInfoCard
@@ -95,7 +113,7 @@ function Information() {
                 />
               </Grid>
             </Grid>
-          </Grid>
+          </Grid> */}
         </Grid>
       </Container>
     </MKBox>
